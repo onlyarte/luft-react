a7f5579474e4deb582d001dd2eebd770334290b9
Ruslan Purii <ruslanpurii@gmail.com>
2018-04-08 10:06:18 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="col-12 col-md-4">
+          <div className="card">
+            <div className="card-body">
+              <div className="text">LUFT // авіаквитки</div>
+            </div>
+          </div>
+        </div>
+        <div className="col-12 col-md-8">
+
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
